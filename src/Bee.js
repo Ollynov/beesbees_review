8597bb95230da3483bb8baa3d0601453986e5c3f
Aljosha Novakovic <ollynov@gmail.com>
2016-12-09 12:40:53 -0800

    Cleaned code and added HoneyMakerBee

--- src/Bee.js
+++ src/Bee.js
@@ -1,31 +1,10 @@
-var Grub = function() {
-  this.age = 0;
-  this.color = 'pink';
-  this.food = 'jelly';
-
-};
-
-Grub.prototype.eat = () => {
-  return 'we eatin bruh!';
-};
-
-
 var Bee = function() {
   Grub.call(this);
   this.age = 5;
   this.color = 'yellow';
   this.job = 'keep on growing';
-  console.log('this is ', this);
 
 };
 
 Bee.prototype = Object.create(Grub.prototype);
 Bee.prototype.constructor = Bee;
-
-Bee.prototype.showColor = function() {
-  console.log('inside the function ', this.color);
-};
-
-var wittleBee = new Bee();
-console.log('ok so wittleBee.color is ', wittleBee.color);
-
